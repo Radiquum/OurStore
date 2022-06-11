af2128b4ae2f1b57a112bb467e42e82bc62b6152
Radiquum <kentai.waah@gmail.com>
2022-06-12 00:36:04 +0500

    minor changes: - [CSS] categories bottom padding "fix" - [Code] better code readability?

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ from flask import Flask, redirect, render_template, send_file, request, abort
 import urllib3
 from urllib.request import Request, urlopen
 
-## STATIC CONFIG ##
-
 app = Flask(__name__)
-
-global headers
-headers = {
-    "content-type": "application/json",
-    "User-Agent": "nashstore",
-    "connection": "close"
-}   
 
 ## FUNCTIONS ##
 
@@ -22,67 +13,102 @@ def api(url):
     if not url.lower().startswith('http'):
         return str("Not a valid url")
     
+    headers = {
+        "content-type": "application/json",
+        "User-Agent": "nashstore",
+        "connection": "close"
+    }   
+    
     try:
         httprequest = Request(url, method="GET", headers=headers)
         with urlopen(httprequest) as response:
             jsonbody = json.load(response)
     except json.JSONDecodeError:
-        abort(404, description="Resource not found")
+        abort(404)
     except HTTPError:
-        abort(400, description="Bad request")
+        abort(400)
             
     return jsonbody
     
 def download(url, name):
     http = urllib3.PoolManager()
     try:
-        return send_file("cache/" + name, as_attachment=True)
+        return send_file(f"cache/{name}", as_attachment=True)
     except FileNotFoundError:
-        with http.request('GET', url, preload_content=False) as r, open("cache/" + name, 'wb') as out_file:       
+        with http.request('GET', url, preload_content=False) as r, open(f"cache/{name}", 'wb') as out_file:       
             shutil.copyfileobj(r, out_file)
-        return send_file("cache/" + name, as_attachment=True)
+        return send_file(f"cache/{name}", as_attachment=True)
 
 ### WEB KINDA ###
 
 @app.route("/")
 def index():
-    return render_template('index.html', page="", title="Главная", description="OurStore - это неофициальный вэб клиент для NashStore, сделанный на Python + Flask")
+    page = {
+        "page": "",
+        "title": "Главная",
+        "description": "OurStore - это неофициальный вэб клиент для NashStore, сделанный на Python + Flask"
+    }
+    return render_template('index.html', page=page)
 
 @app.route("/applications")
 @app.route("/applications/<id>")
 def applications(id=None):
-    if (id != None):
-        j = api("https://store.nashstore.ru/api/mobile/v1/categories/application/" + id)
-        return render_template("category.html", j = j, page="applications/" + id, title="Приложения > " + j['list'][0].get('category'))
+    
+    if (id is not None):
+        j = api(f"https://store.nashstore.ru/api/mobile/v1/categories/application/{id}")
+        page = {
+            "page": f"applications/{id}",
+            "title": f"Приложения > {j['list'][0].get('category')}",
+        }   
+        return render_template("category.html", j = j, page=page)
+    
+    page = {
+        "page": "applications",
+        "title": "Приложения",
+    }
     
     j = api("https://store.nashstore.ru/api/mobile/v1/categories/application")
-    return render_template("available_categories.html", j = j, page="applications", title="Приложения")
+    return render_template("available_categories.html", j = j, page=page)
 
 @app.route("/games")
 @app.route("/games/<id>")
 def games(id=None):
-    if (id != None):
+    if (id is not None):
         j = api("https://store.nashstore.ru/api/mobile/v1/categories/game/" + id)
-        return render_template("category.html", j = j, page="games/" + id, title="Игры > " + j['list'][0].get('category'))
+        page = {
+            "page": f"applications/{id}",
+            "title": f"Игры > {j['list'][0].get('category')}",
+        }   
+        return render_template("category.html", j = j, page=page)
+    
+    page = {
+        "page": "games",
+        "title": "Игры",
+    }
     
     j = api("https://store.nashstore.ru/api/mobile/v1/categories/game")
-    return render_template("available_categories.html", j = j, page="games", title="Игры")
+    return render_template("available_categories.html", j = j, page=page)
 
 @app.route("/application")
 @app.route("/application/<id>")
 def apk (id=None):
-    if (id == None):
+    if (id is None):
         return redirect("/")
     
-    j = api("https://store.nashstore.ru/api/mobile/v1/application/" + id)
-    s = json.dumps(j['app']['screens'])
-    s = json.loads(s)
-    return render_template("application.html", j = j, s=s, page="application/" + id, title=j['app'].get('title'), description=j['app'].get('short_description'), image=j['app'].get('icon') + "/original")
+    j = api(f"https://store.nashstore.ru/api/mobile/v1/application/{id}")
+    page = {
+        "page": f"application/{id}",
+        "title": j['app'].get('title'),
+        "description": j['app'].get('short_description'), 
+        "image": f"{j['app'].get('icon')}/original"
+    } 
+    s = j['app']['screens']
+    return render_template("application.html", j = j, s=s, page=page)
 
 @app.route("/download")
 def get_application ():
     try:
-        url = "https://cdnb.nashstore.ru/store/install/" + request.args.get('url')
+        url = f"https://cdnb.nashstore.ru/store/install/{request.args.get('url')}"
         name = request.args.get('name')
         return download(url, name)
     except TypeError:
@@ -100,7 +126,6 @@ def forbidden(e):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status, this is what it catches
     return render_template('error.html', code="404", description="Not Found"), 404 
 
 @app.errorhandler(500)
